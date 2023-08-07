e5b7c8aa84e46b5508ccc6f02072e62ef3b5928c
Intakhab Alam <intakhabalam2004@gmail.com>
2023-08-07 18:40:36 +0530

    Update Main.java

--- src/main/com/STBAproject/Main.java
+++ src/main/com/STBAproject/Main.java
@@ -6,5 +6,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Intakhab Alam");
+        System.out.println("Student-Teacher-Appointment-Booking");
     }
 }
