2cefe8ccac519164e4938057cb90c3d3bbae2584
purna9547 <pradhan.purna2017@gmail.com>
2023-08-07 19:37:59 +0530

    Update Purna.java

--- src/main/com/STBAproject/Purna.java
+++ src/main/com/STBAproject/Purna.java
@@ -1,10 +1,7 @@
 public class Purna {
     public static void main(String[] args) {
         System.out.println("Heloo purna");
-<<<<<<< Updated upstream
         System.out.println("Intakhab Alam");
-=======
         System.out.println("Hello World");
->>>>>>> Stashed changes
     }
 }
