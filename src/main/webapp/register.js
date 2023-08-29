d955f8d9e7e1ad0405f805b65a0d18ecbe5d896e
MrCheseBall <sekhaship960@gmail.com>
2023-08-29 21:25:23 +0530

    Registerion page upload

--- src/main/webapp/register.js
+++ src/main/webapp/register.js
@@ -1,29 +1,12 @@
- function validateForm() {
-            var inputs = document.getElementsByTagName("input");
 
-            for (var i = 0; i < inputs.length; i++) {
-                if (inputs[i].type === "text" || inputs[i].type === "password") {
-                    if (inputs[i].value === "") {
-                        alert("Please fill out all fields");
-                        return false;
-                    }
-                }
-            }
+function validateForm() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
 
-            var password = document.getElementById("password").value;
-            var confirmPassword = document.getElementById("confirmPassword").value;
-            var passwordError = document.getElementById("passwordError");
+    if (password !== confirmPassword) {
+      alert("Password does not match the confirm password.");
+      return false;
+    }
+    return true;
+  }
 
-            if (password !== confirmPassword) {
-                passwordError.style.display = "block";
-                return false;
-            }
-
-            passwordError.style.display = "none";
-            return true;
-        }
-
-        function clearPasswordError() {
-            var passwordError = document.getElementById("passwordError");
-            passwordError.style.display = "none";
-        }
